bb95dee5e9abbe2fb2f01222b098bc6028ce3261
zxcv859500 <zxcv859500@naver.com>
2019-12-28 21:34:51 +0900

    Variables class 화

---
+++ src/controller/components.js
@@ -0,0 +1,7 @@
+module.exports = {
+      parser(componentLst) {
+          for (let component of componentLst) {
+
+          }
+      }
+};
--- src/controller/variables.js
+++ src/controller/variables.js
@@ -1,4 +1,37 @@
-module.exports = {
+/**
+ *
+ * @type {module.Variables}
+ */
+module.exports = class Variables {
+    constructor(varLst) {
+        let variables = {};
+
+        // Type: int_random or float_random부터 처리
+        for (const data of varLst) {
+            if (data['type'] === 'int_random') {
+                variables[data['name']] = Variables.int_random(data['deciamlScope']);
+            }
+
+            else if (data['type'] === 'float_random') {
+                variables[data['name']] = Variables.float_random(data['decimalScope'], data['fix']);
+            }
+        }
+
+        // 나머지 eval
+        for (const data of varLst) {
+            if (data['type'] === 'eval') {
+                const script = Variables.eval_script(data['value'], data['props']);
+                variables[data['name']] = eval(script);
+            }
+        }
+
+        this.variables = variables;
+    }
+
+    get var() {
+        return this.variables;
+    }
+
 
     /**
      * 정수 범위 난수
@@ -6,36 +39,36 @@ module.exports = {
      * @param decimalScope 범위
      * @return 정수 난수
      */
-    int_random(decimalScope) {
+    static int_random(decimalScope) {
         const min = decimalScope[0];
         const max = decimalScope[1];
 
         return Math.floor(Math.random()*(max-min+1)) + min;
-    },
+    }
 
     /**
      * 실수 범위 난수
      *
      * @param decimalScope 범위
      * @param fix 소수 자리 수
-     * @retrun 실수 난수
+     * @returns 실수 난수
      */
-    float_random(decimalScope, fix) {
+    static float_random(decimalScope, fix) {
         const min = decimalScope[0];
         const max = decimalScope[1];
 
         return (Math.random()*(max-min+1) + min).toFixed(fix)
-    },
+    }
 
     /**
      * type: eval 의 스크립트 처리
      *
      * @param script evalScript
      * @param props evalScript 에 쓰이는 변수들 리스트
-     * @returns 처리된 eval script
+     * @returns string
      */
-    // TODO: 해당 prop이 없을 때 예외 발생
-    eval_script(script, props) {
+// TODO: 해당 prop이 없을 때 예외 발생
+    static eval_script(script, props) {
         let defScript = '';
 
         for (const prop of props) {
@@ -43,30 +76,5 @@ module.exports = {
         }
 
         return defScript + script;
-    },
-
-    parser(varLst) {
-        let variables = {};
-
-        // Type: int_random or float_random부터 처리
-        for (const data of varLst) {
-            if (data['type'] === 'int_random') {
-                variables[data['name']] = this.int_random(data['deciamlScope']);
-            }
-
-            else if (data['type'] === 'float_random') {
-                variables[data['name']] = this.float_random(data['decimalScope'], data['fix']);
-            }
-        }
-
-        // 나머지 eval
-        for (const data of varLst) {
-            if (data['type'] === 'eval') {
-                const script = this.eval_script(data['value'], data['props']);
-                variables[data['name']] = this.eval(script);
-            }
-        }
-
-        return variables;
     }
 };
