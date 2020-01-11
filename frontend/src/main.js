6af1fe5d1a884d554cef8f708aedfac006ec7bc7
zxcv859500 <zxcv859500@naver.com>
2020-01-11 16:08:19 +0900

    vuex init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,10 @@ import './plugins/axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import Vuex from 'vuex'
 
 Vue.use(ElementUI)
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
